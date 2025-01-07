359a6f9c9fcdfbb613f65ee5fc35b084c16ada09
Pozzi <eanderea1@gmail.com>
2025-01-07 18:52:53 -0300

    Update Articles.vue

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -7,16 +7,24 @@
         <div class="carousel__wrapper">
           <carousel :navigationEnabled="true">
           <slide>
-            Slide 1 Content
+            <div class="slide-content">
+              <img class="slide-image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc0.wallpaperflare.com%2Fpreview%2F111%2F43%2F764%2Fcode-coder-codes-coding.jpg&f=1&nofb=1&ipt=49602ef1f889e888a5f2277197ff8bc03127e1749b13301a60c9b661d4093191&ipo=images">
+            </div>
           </slide>
           <slide>
-            Slide 2 Content
+            <div class="slide-content">
+              <img class="slide-image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.bacancytechnology.com%2Fblog%2Fwp-content%2Fuploads%2F2023%2F05%2FAngular-16.png&f=1&nofb=1&ipt=0556f1a0e290232e5ec58b0cf1e27b6665c4f1d335022ba0c01ee7a65d045efc&ipo=images">
+            </div>
           </slide>
           <slide>
-            Slide 3 Content
+            <div class="slide-content">
+              <img class="slide-image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fik.imagekit.io%2Flaxaar%2F1686313855890ReactJS_2400x1200.png&f=1&nofb=1&ipt=eb487fa90df7337079a67e27c4b9f6dbb7a6f005651cb697f544f1b428ec7bd8&ipo=images">
+            </div>
           </slide>
           <slide>
-            Slide 4 Content
+            <div class="slide-content">
+              <img class="slide-image" src="https://iglu.net/wp-content/uploads/2022/12/What-is-Golang.png">
+            </div>
           </slide>
         </carousel>
         </div>
@@ -100,6 +108,19 @@ export default class About extends Vue {
     color: #b9b9b9;
   }
 
+  .slide-content {
+    border: 2px solid #232323;
+    height: 316px;
+    margin-left: 30px;
+    margin-right: 30px;
+    border-radius: 10px;
+  }
+
+  .slide-image{
+    width: 100%;
+    height: 100%;
+  }
+
   @media screen and (max-width: 1200px) {
     .profile {
       display: none;
@@ -120,6 +141,19 @@ export default class About extends Vue {
     display: flex;
     font-size: 1.6em;
   }
+
+</style>
+
+<style>
+  .VueCarousel-navigation-button {
+    color: white !important;
+    font-size: 36px;
+    margin-top: -40px;
+  }
+  .VueCarousel-navigation-button:focus {
+    outline: none !important;
+  }
+  
 </style>
 
 
